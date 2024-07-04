a2ae51d3bf278c1b6fafd648e932f529ad3b90a6
Murilo-Gotardo <murilogotardo08@gmail.com>
2024-07-04 11:21:14 -0300

    feat: Add automatic local machine ip binding

--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,29 @@ extern crate core;
 use std::io::Read;
 use std::net::{TcpListener, TcpStream};
 use std::thread;
+use local_ip_address::local_ip;
 use crate::requisition::Requisition;
 mod requisition;
 mod commands;
 mod json_sender;
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:11000");
-    
-    for stream in listener.unwrap().incoming() {
-        match stream {
-            Ok(stream) => {
-                thread::spawn(|| {
-                    resolve_requisition(stream);
-                });
-            } Err(e) => println!("deu ruim")
-        }
+    match local_ip() {
+        Ok(ip) => {
+            println!("Running on {}:11000", ip);
+            let listener = TcpListener::bind(ip.to_string() + ":11000");
+
+            for stream in listener.unwrap().incoming() {
+                match stream {
+                    Ok(stream) => {
+                        thread::spawn(|| {
+                            resolve_requisition(stream);
+                        });
+                    } Err(e) => println!("deu ruim")
+                }
+            }
+        },
+        Err(e) => eprintln!("Falha ao pegar ip local: {}", e),
     }
     
     Ok(())
@@ -33,7 +40,6 @@ fn resolve_requisition(mut connection: TcpStream) {
     connection.read(&mut buffer).expect("TODO: panic message");
   
     let json = String::from_utf8_lossy(&buffer[..]);
-    println!("{}", json);
     let cleaned_json = json.trim_end_matches('\0');
     let requisition = serde_json::from_str::<Requisition>(&cleaned_json).expect("cwe");
     
